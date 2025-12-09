00cfcf968b5f3ec161719cf0f0a4bb21bfc8ceb5
Jon Zehnder <jonzehnder@me.com>
2025-12-09 20:46:18 +0100

    remove led layer

--- boards/shields/aek_min/leds.c
+++ boards/shields/aek_min/leds.c
@@ -42,23 +42,23 @@ static int led_keylock_listener_cb(const zmk_event_t *eh) {
 ZMK_LISTENER(led_indicators_listener, led_keylock_listener_cb);
 ZMK_SUBSCRIPTION(led_indicators_listener, zmk_hid_indicators_changed);
 
-// Layer state listener for layer #1 LED
-static int led_layer_listener_cb(const zmk_event_t *eh) {
-  const struct zmk_layer_state_changed *ev = as_zmk_layer_state_changed(eh);
+// // Layer state listener for layer #1 LED
+// static int led_layer_listener_cb(const zmk_event_t *eh) {
+//   const struct zmk_layer_state_changed *ev = as_zmk_layer_state_changed(eh);
 
-  // layer 2 -> NUM
-  if (ev->layer == 1) {
-    if (ev->state) {
-      led_on(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_layer)));
-    } else {
-      led_off(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_layer)));
-    }
-  }
-  return 0;
-}
+//   // layer 2 -> NUM
+//   if (ev->layer == 1) {
+//     if (ev->state) {
+//       led_on(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_layer)));
+//     } else {
+//       led_off(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_layer)));
+//     }
+//   }
+//   return 0;
+// }
 
-ZMK_LISTENER(layer_led_listener, led_layer_listener_cb);
-ZMK_SUBSCRIPTION(layer_led_listener, zmk_layer_state_changed);
+// ZMK_LISTENER(layer_led_listener, led_layer_listener_cb);
+// ZMK_SUBSCRIPTION(layer_led_listener, zmk_layer_state_changed);
 
 static int leds_init(void) {
   if (!device_is_ready(led_dev)) {
